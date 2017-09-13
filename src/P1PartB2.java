5da4088fbae9f16e0cffd272a54ec7f632db5cef
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-09-12 21:04:29 -0600

    Started Work on ReverseDigit Function
    Created file for class and Built function skeleton

---
+++ src/P1PartB2.java
@@ -0,0 +1,24 @@
+// Program to calculate the Reverse Digit of a positive Integer
+
+public class P1PartB2 {
+	
+    public static void main(String[] args) {
+        long X, Y;
+		    if(args.length >= 1){
+		      X = Long.parseLong(args[0]);
+		      if(X >= 1){
+		        Y = ReverseDigits(X);
+		        System.out.println("\n X = " + X + "     Y = " + Y);
+		      }
+		    }
+		    return;
+	} // end main
+    
+    public static long ReverseDigits(long x) {
+    	long y = 0;
+    	
+    	
+    	return y;
+    } // end Reverse Digits
+
+} // end class
