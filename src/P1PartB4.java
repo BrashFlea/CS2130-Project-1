86f8c237578eb98e58475b9f1df47f75ac0ed034
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-09-15 16:47:56 -0600

    Completed BaseB Function
    Tested and validated against project inputs

--- src/P1PartB4.java
+++ src/P1PartB4.java
@@ -3,23 +3,32 @@
 public class P1PartB4 {
     
     public static void main(String[] args) {
-        long X, B, Y;
+        long X, B;
             if(args.length >= 2){
               X = Long.parseLong(args[0]);
               B = Long.parseLong(args[1]);
-              if( (X >= 1 && X <= 100000) && (B >= 1 && B <= 10) ){
-                  Y = BaseB(X, B);
-                System.out.println("\n X = " + X + "     B = " + B + "     Y = " + Y);
+              if( (X >= 1 && X <= 100000) && (B >= 2 && B <= 10) ){
+                  System.out.print("\n X = " + X + "     B = " + B + "     Y = ");
+                  BaseB(X, B);
               }
             }
             return;
     } // end main
     
-    public static long BaseB(long x, long b) {
-        long y = 0;
-        
-            
-        return y;
+    public static void BaseB(long x, long b) {
+
+        // base case
+        if (x == 0) {
+            return;
+        }
+        long y = x % b;
+        x /= b;
+        if (y < 0) {
+            x += 1;
+        }
+        BaseB(x,b);
+        System.out.print(y); 
+        return;
     } // end BaseB
 
 } // end class
